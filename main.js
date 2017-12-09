6d44ec04caa9e307a992feb1694477f320135478
MilandaYuan <nwhals@163.com>
2017-12-09 17:17:54 +0800

    modify mistakes of the first review

--- main.js
+++ main.js
@@ -1,146 +1,104 @@
+let initialLocations;
 
-var initialLocations = [
-    {
-        title: '天安门',
-        pinyin: 'tiananmen',
-        location: {lat: 39.90872, lng: 116.39748},
-        id: 'ChIJtZy9k79S8DURURn7o3wj30Q'
-    },
-    {
-        title: '香山公园',
-        pinyin: 'xiangshangongyuan',
-        location: {lat: 39.9926176, lng: 116.1867996},
-        id: 'ChIJIc2UcK9b8DURcnVb7tVrF7o'
-    },
-    {
-        title: '尤伦斯当代艺术中心',
-        pinyin: 'youlunsidangdaiyishuzhongxin',
-        location: {lat: 39.9838588, lng: 116.4951909},
-        id: 'ChIJl3OsUL-r8TURjRjdvXQ_TTE'
-    },
-    {
-        title: '国家体育场',
-        pinyin: 'guojiatiyuchang',
-        location: {lat: 39.9929431, lng: 116.3965112},
-        id: 'ChIJ-3xmbcJU8DURR-pxV2SC8jA'
-    },
-    {
-        title: '北京动物园',
-        pinyin: 'beijingdongwuyuan',
-        location: {lat: 39.93879400000001, lng: 116.339571},
-        id: 'ChIJPRYRTDJS8DUR5fFsHhL_tW4'
-    },
-    {
-        title: '中国人民大学',
-        pinyin: 'zhongguorenmindaxue',
-        location: {lat: 39.9696062, lng: 116.3188145},
-        id: 'ChIJjSNcWFtR8DURiHGXEcrhDbg'
-    },
-    {
-        title: '麦当劳牡丹园店',
-        pinyin: 'maidanglaomudanyuandian',
-        location: {lat: 39.9756227, lng: 116.3696116},
-        id: 'ChIJod0J44BU8DURpZk97WVBJ7Q'
+$.ajax({
+    url: 'json/data.json',
+    dataType: 'json',
+    async: true,
+    success: function (response) {
+        initialLocations = response;
     }
 
-];
+});
 
 
 //动态绑定原始数据
-var Location = function(data){
-    this.title = ko.observable(data.title)
-};
-var ViewModel = function(){
-    var self = this;
+const ViewModel = function () {
     this.locationList = ko.observableArray([]);
     this.inputLocation = ko.observable('');
-    initialLocations.forEach(function(locationItem){
-        self.locationList.push(new Location(locationItem))
-    });
+    initialLocations.forEach(locationItem => this.locationList.push(locationItem));
+    this.showClickedMarker = ele => showClickedMarker(ele.title);
 
-    this.inputLocation.subscribe(function(newValue){
-        var filteredLocation = initialLocations.filter(function(ele){
+    this.inputLocation.subscribe(newValue => {
+        const filteredLocation = initialLocations.filter(function (ele) {
             return ele.title.includes(newValue) || ele.pinyin.includes(newValue);
         });
-        self.locationList(filteredLocation);
+        this.locationList(filteredLocation);
         showFilteredMarkers(filteredLocation);
     });
+
+    //点击汉堡菜单切换左侧地址列表的显示和隐藏状态
+    this.toggleStatus = ko.observable(false);
+    this.toggleSidebar = () => {
+        this.toggleStatus(!this.toggleStatus());
+        google.maps.event.trigger(map, 'resize');
+    }
 };
 
 ko.applyBindings(new ViewModel());
 
 
-//给左侧地址列表绑定点击事件，以显示右侧对应的标记
-var locationItems = document.getElementsByClassName('location-item');
-for(var i =0;i<locationItems.length;i++){
-    var currentLocation =locationItems[i];
-    currentLocation.addEventListener('click',function(){
-        showClickedMarker(this)
-    })
-}
-
-
-var map;
-var markers = [];
+let map;
+const markers = [];
 
 //初始化加载地图
-$.getScript('https://maps.googleapis.com/maps/api/js?libraries=places&key=[YOUR_API_KEY]&v=3')
-    .done(function initMap(response){
-        map = new google.maps.Map(document.getElementById('map'),{
-            center:{lat: 39.90872, lng: 116.39748},
-            zoom:12,
-            mapTypeControl: false
-        });
-
-        var largeInfowindow = new google.maps.InfoWindow();
-        var bounds = new google.maps.LatLngBounds();
-
-        //初始化时让所有标记显示在页面上
-
-        for(var i=0;i<initialLocations.length;i++){
-            var title = initialLocations[i].title;
-            var position = initialLocations[i].location;
-            var placeID = initialLocations[i].id;
-            var marker = new google.maps.Marker({
-                map:map,
-                title:title,
-                position:position,
-                animation:google.maps.Animation.DROP,
-                placeID:placeID
+$.getScript('https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyBsP09nfFtiN4v2d_Nd6uUPKKNMn6B9ow0&v=3')
+    .done(function initMap(response) {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 39.90872, lng: 116.39748},
+                zoom: 12,
+                mapTypeControl: false
             });
-            markers.push(marker);
-            marker.addListener('click',function(){
-                toggleBounce(this);
-                populateInfoWindow(this,largeInfowindow);
-            });
-            bounds.extend(markers[i].position);
-            map.fitBounds(bounds);
+
+            let largeInfowindow = new google.maps.InfoWindow({maxWidth: 250, font: 10});
+            let bounds = new google.maps.LatLngBounds();
+
+            //初始化时让所有标记显示在页面上
+            for (let i = 0; i < initialLocations.length; i++) {
+                const title = initialLocations[i].title;
+                const position = initialLocations[i].location;
+                const placeID = initialLocations[i].id;
+                const wiki = initialLocations[i].wiki;
+                const marker = new google.maps.Marker({
+                    map: map,
+                    title: title,
+                    position: position,
+                    animation: google.maps.Animation.DROP,
+                    placeID: placeID,
+                    wiki: wiki
+                });
+                markers.push(marker);
+                marker.addListener('click', function () {
+                    toggleBounce(this);
+                    populateInfoWindow(this, largeInfowindow);
+                });
+                bounds.extend(markers[i].position);
+                map.fitBounds(bounds);
+            }
         }
-    }
-)
-    .fail(function(){
-        $('#map').toggleClass('map-error').text('Oops~~~获取地图失败，请检查您的网络') ;
+    )
+    .fail(function () {
+        $('#map').toggleClass('map-error').text('Oops~~~获取地图失败，请检查您的网络');
 
     })
 
 //填充被点击标记的信息窗口内容
-function populateInfoWindow(marker,infowindow){
-    if(infowindow.marker !=marker){
+function populateInfoWindow(marker, infowindow) {
+    if (infowindow.marker !== marker) {
         infowindow.marker = marker;
-        var service = new google.maps.places.PlacesService(map);
+        const service = new google.maps.places.PlacesService(map);
         service.getDetails({
-            placeId:marker.placeID
-        },function(place,status){
-            if(status ===google.maps.places.PlacesServiceStatus.OK){
-                var innerHTML = '<div>';
-                if(place.name){
-                    innerHTML+='<strong>'+place.name+'</strong>';
+            placeId: marker.placeID
+        }, function (place, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                let innerHTML = '<div>';
+                if (place.name) {
+                    innerHTML += '<strong>' + place.name + '</strong>';
                 }
-                if(place.formatted_address){
-                    innerHTML+='<br>'+place.formatted_address
+                if (place.formatted_address) {
+                    innerHTML += '<br>' + place.formatted_address
                 }
-                if(place.formatted_phone_number){
-                    innerHTML+='<br>'+place.formatted_phone_number
+                if (place.formatted_phone_number) {
+                    innerHTML += '<br>' + place.formatted_phone_number
                 }
                 if (place.opening_hours) {
                     innerHTML += '<br><br><strong>Hours:</strong><br>' +
@@ -154,18 +112,18 @@ function populateInfoWindow(marker,infowindow){
                 }
                 if (place.photos) {
                     innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
-                            {maxHeight: 100, maxWidth: 200}) + '">';
+                        {maxHeight: 100, maxWidth: 200}) + '">';
                 }
 
-                innerHTML += '</div>';
+                innerHTML += '</div></br>';
                 infowindow.setContent(innerHTML);
-                infowindow.open(map,marker);
-                infowindow.addListener('closeclick',function(){
+                infowindow.open(map, marker);
+                infowindow.addListener('closeclick', function () {
                     infowindow.marker = null;
-                })
+                });
 
-                getWikipediaContent(place,function(content){
-                    innerHTML+=content;
+                getWikipediaContent(marker, function (content) {
+                    innerHTML += content;
                     infowindow.setContent(innerHTML);
                 });
 
@@ -176,69 +134,65 @@ function populateInfoWindow(marker,infowindow){
 }
 
 //获取维基百科的内容
-function getWikipediaContent(place,callback){
-    var wikiUrl = 'http://zh.wikipedia.org/w/api.php?action=opensearch&search=' + place.name + '&format=json&callback=wikiCallback';
+function getWikipediaContent(marker, callback) {
+    const wikiUrl = 'http://zh.wikipedia.org/w/api.php?action=opensearch&search=' + (marker.wiki || marker.title) + '&format=json&callback=wikiCallback';
     $.ajax({
-        url:wikiUrl,
-        dataType:'jsonp',
-        jsonp:'callback',
+        url: wikiUrl,
+        dataType: 'jsonp',
+        jsonp: 'callback',
         timeout: 3000,
-        success:function(response){
-            var url = 'http://zh.wikipedia.org/wiki/'+place.name;
-            if(response[1].length===0){
+        success: function (response) {
+            const url = 'http://zh.wikipedia.org/wiki/' + (marker.wiki || marker.title);
+            if (response[1].length === 0) {
                 callback('')
-            }else{
-                callback( '<div>'+response[2][0]+'<a href="'+url+'" target ="_blank">详见维基百科</a>'+'</div>')
+            } else {
+                callback(`<div>${response[2][0]}<a href=${url} target ="_blank">详见维基百科</a></div>`)
             }
         },
-        error: function(req, errorMessage, e) {
+        error: function (req, errorMessage, e) {
             callback("<div>Failed to get wikipedia resources</div>")
         }
     });
 }
 
 //切换标记的弹跳动画
-function toggleBounce(marker){
-    if(marker.getAnimation() !==null){
+function toggleBounce(marker) {
+    if (marker.getAnimation() !== null) {
         marker.setAnimation(null)
-    }else{
+    } else {
         marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function(){ marker.setAnimation(null); }, 3000);
+        setTimeout(function () {
+            marker.setAnimation(null);
+        }, 3000);
     }
 }
 
 //显示被点击的地址对应标记的弹出窗口
-function showClickedMarker(clickedLocation){
-    for(var i =0;i<markers.length;i++){
-        var marker = markers[i];
-        var largeInfowindow = new google.maps.InfoWindow();
-        if (clickedLocation.innerText===marker.title) {
+function showClickedMarker(clickedLocation) {
+    for (let marker of markers) {
+        let largeInfowindow = new google.maps.InfoWindow({maxWidth: 250});
+        if (clickedLocation === marker.title) {
             toggleBounce(marker);
-            populateInfoWindow(marker,largeInfowindow);
+            populateInfoWindow(marker, largeInfowindow);
         }
     }
 }
 
 //根据输入内容筛选出对应的标记
-function showFilteredMarkers(filtered){
+function showFilteredMarkers(filtered) {
     markers.forEach(function (m) {
-        var needDisplay = filtered.some(function (e){
+        let needDisplay = filtered.some(function (e) {
             return e.title === m.title;
         });
-        if(needDisplay){
-            m.setMap(map);
-        }else{
-            m.setMap(null);
+        if (needDisplay) {
+            m.setVisible(true)
+        } else {
+            m.setVisible(false);
         }
     });
 }
 
-//点击汉堡菜单切换左侧地址列表的显示和隐藏状态
-var menuIcon = $('.menuIcon');
-menuIcon.on('click',function(){
-    $('.container').toggleClass('menu-hidden');
-    google.maps.event.trigger(map, 'resize');
-});
+
 
 
 
